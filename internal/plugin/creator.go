4270bfd7e605b1f8ac11cdf2f7add0e337b8cb1f
storezhang <storezhang@gmail.com>
2023-10-25 21:00:25 +0800

    refactor(项目): 基于最新pangum/pangu接入

--- internal/plugin/creator.go
+++ internal/plugin/creator.go
@@ -1,8 +1,8 @@
 package plugin
 
 import (
+	"github.com/goexl/log"
 	"github.com/goexl/schedule"
-	"github.com/goexl/simaqian"
 	"github.com/pangum/pangu"
 )
 
@@ -10,7 +10,7 @@ type Creator struct {
 	// 解决命名空间问题
 }
 
-func (c *Creator) New(config *pangu.Config, logger simaqian.Logger) (scheduler *schedule.Scheduler, err error) {
+func (c *Creator) New(config *pangu.Config, logger log.Logger) (scheduler *schedule.Scheduler, err error) {
 	wrapper := new(Wrapper)
 	if ge := config.Build().Get(wrapper); nil != ge {
 		err = ge
@@ -21,7 +21,7 @@ func (c *Creator) New(config *pangu.Config, logger simaqian.Logger) (scheduler *
 	return
 }
 
-func (c *Creator) new(config *Config, logger simaqian.Logger) (scheduler *schedule.Scheduler, err error) {
+func (c *Creator) new(config *Config, logger log.Logger) (scheduler *schedule.Scheduler, err error) {
 	builder := schedule.New().Logger(logger)
 	if config.Unique {
 		builder.Unique()
